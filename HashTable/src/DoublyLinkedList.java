f103fad9c12be3413bce08b205d86da5366bcf46
philipmorrone <philip.j.morrone@gmail.com>
2019-04-11 01:29:41 -0400

    Adding Doubly Linked List

---
+++ HashTable/src/DoublyLinkedList.java
@@ -0,0 +1,45 @@
+
+public class DoublyLinkedList {
+	 private Node head;
+	 private Node tail;
+	 private int size = 0;
+
+	 private static class Node {
+		 private Student stud;
+	     private Node next;
+	     private Node prev;
+
+	 public Node(Student s, Node nxt, Node prv) {
+		 stud = s;
+	     next = nxt;
+	     prev = prv;
+	     }
+	 
+    public Node(Student s) {
+	     stud = s;
+	     next = null;
+	     prev = null;
+	     }
+	}
+	 
+	 public void addTail(Student stud) {
+	        Node newNode = new Node(stud, null, null);
+	        if (size == 0) {
+	            head = tail = newNode;
+	        } else {
+	            newNode.prev = tail;
+	            tail.next = newNode;
+	            tail = newNode;
+	        }
+	        size++;
+	}
+	 
+	 public void print() {
+	        Node temp = head;
+	        while (temp != null) {
+	            System.out.print(temp.stud + " ");
+	            temp = temp.next;
+	        }
+	    }
+
+}
